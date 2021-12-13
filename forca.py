38f0d2f012a702c482c08c1f5bb76566cadc09e3
Bigarato12 <contato.joaobigarato@gmail.com>
2021-12-12 21:54:35 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,10 @@
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+if(__name__=="___main__"):
+    jogar()
